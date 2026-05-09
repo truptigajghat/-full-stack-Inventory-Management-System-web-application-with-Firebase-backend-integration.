5c9a965c4484e8f9fb0e9782dc79fcf95f5ff134
truptigajghat <truptigajghat18@gmail.com>
2026-05-09 14:45:16 +0530

    Fix: Update to user's personal Firebase project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,6 +5,6 @@ import { getStorage } from 'firebase/storage';
 import firebaseConfig from '../../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
